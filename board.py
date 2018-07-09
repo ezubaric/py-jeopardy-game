86a5c43badb55df97b9412c8b96abf291c8bc429
Jordan Boyd-Graber <jordanbg@gmail.com>
2018-07-09 17:58:43 -0400

    scoring and selecting working, need to get lockout working

--- board.py
+++ board.py
@@ -1,4 +1,7 @@
 import pygame
+import textwrap
+import os
+from random import random
 from collections import defaultdict
 
 kCOLORS = {}
@@ -11,24 +14,159 @@ kCOLORS["black"] = (0, 0, 0)
 
 kMONEY = {1: [200, 400, 600, 800, 1000],
           2: [400, 800, 1200, 1600, 2000]}
+kNUM_ROWS = len(kMONEY[1])
+kCOLUMN_LETTERS = "ABCDEF"
+
+
+# Utilities for single character input
+class _Getch:
+    """Gets a single character from standard input.  Does not echo to the
+screen."""
+    def __init__(self):
+        try:
+            self.impl = _GetchWindows()
+        except ImportError:
+            self.impl = _GetchUnix()
+
+    def __call__(self): return self.impl()
+
+
+class _GetchUnix:
+    def __init__(self):
+        import tty, sys
+
+    def __call__(self):
+        import sys, tty, termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+            sys.stdin.flush()
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+
+class _GetchWindows:
+    def __init__(self):
+        import msvcrt
+
+    def __call__(self):
+        import msvcrt
+        return msvcrt.getch()
+
+getch = _Getch()
+
+
+def game_loop(scores):
+    # initialize the board
+    pane = Pane()
+    pane.draw_grid(scores, 1)
+    pane.load_questions("study.csv")
+    pane.start_round(1)
+    pane.display_text()
+    while pane.clues_left() > 0:
+        question = pane.get_clue()
+        print("Selection", question)
+        print("Category", pane.categories[question[0]])
+        player = pane.buzz(question)
+        print(kMONEY, question[1], question)
+        scores = pane.result(scores, player, pane.board_values[question[1]], question)
+
+        pane.draw_grid(scores, player)
+        pane.display_text()
+        
+
 
 class Pane(object):
     def __init__(self, width=1200, height=800, row_height=100, num_columns=6,
-                     text_offset_x=10, text_offset_y=10):
+                     text_offset_x=10, text_offset_y=10, text_line_height=20,
+                     char_width=15):
         pygame.init()
         self.text_offset_x = text_offset_x
+        self.char_width = char_width
+        self.text_line_height = text_line_height
         self.text_offset_y = text_offset_y
         self.width = width
         self.height = height
         self.num_columns = num_columns
+        self.board = {}
         self.row_height = row_height
         self.font = pygame.font.SysFont('Arial', 18)
-        pygame.display.set_caption('Box Test')
+        pygame.display.set_caption('Jeopardy Tester')
         self.screen = pygame.display.set_mode((width, height), 0, 32)
-        self.screen.fill((kCOLORS["white"]))
+        self.screen.fill((kCOLORS["blue"]))
         self.draw_grid_flag=True
         pygame.display.update()
 
+    def result(self, score, player, value, question):
+        x, y = question
+        
+        print("Answer: %s" % self.board[x][y]["answer"])
+        char = " "
+        while char not in "+-":
+            char = getch()
+        if char == "+":
+            score[player] += value
+        elif char == "-":
+            score[player] -= value
+
+        self.board[x][y] = None
+            
+        return score
+
+    def buzz(self, question):
+        self.screen.fill((kCOLORS["blue"]))
+        x, y = question
+        pygame.display.update()
+
+        font = pygame.font.SysFont("times", 50)
+        row = 0
+        for ii in textwrap.wrap(self.board[x][y]["clue"], 40):
+            row += 1
+            text = font.render(ii, True, kCOLORS["white"])
+            self.screen.blit(text, (.5 * self.width // self.num_columns, self.row_height * row))
+        pygame.display.update()
+
+        os.system('say "%s"' % self.board[x][y]["clue"].replace('"', ''))
+
+        sleep(random())
+        keys=pygame.key.get_pressed()
+        print(keys)
+        
+        self.screen.fill((kCOLORS["yellow"]))
+        pygame.display.update()
+        
+        char = "q"
+        while char not in " 1234567890":
+            char = getch()
+            print("Keypress: %s" % char)
+
+        if char == " ":
+            return -1
+        else:
+            os.system("say 'Player %s'" % char)
+            return int(char)
+
+
+        
+    def clues_left(self):
+        return sum(1 for x in self.board if self.board[x] is not None)
+
+    def get_clue(self):
+        square = input("Select a square:")
+        while len(square) != 2 or square[0] not in kCOLUMN_LETTERS or int(square[1]) not in range(1, 7):
+            square = input("Select a square:")
+
+        
+        x = kCOLUMN_LETTERS.index(square[0])
+        y = int(square[1]) - 1
+        if x not in self.board or y not in self.board[x] or self.board[x][y] is None:
+            return self.get_clue()
+        else:
+            return x, y
+        
     def start_round(self, round):
         from random import sample
         self.board_values = kMONEY[round]
@@ -38,13 +176,12 @@ class Pane(object):
         print("Using categories", self.categories)
         self.board = defaultdict(dict)
 
-        column = 0
-        for ii in self.categories:
-            column += 1
+        for column, ii in enumerate(self.categories):
             row = 0
             for jj in sorted(self.questions[round][ii]):
-                row += 1
                 self.board[column][row] = self.questions[round][ii][jj]
+                row += 1
+            
 
     def load_questions(self, file):
         from collections import defaultdict
@@ -62,39 +199,39 @@ class Pane(object):
                 
         
     def draw_grid(self, score, team):
-        if self.draw_grid_flag:
-            self.screen.fill((kCOLORS["white"]))    
-            self.rect = pygame.draw.rect(self.screen, (kCOLORS["blue"]), 
+        self.screen.fill((kCOLORS["blue"]))    
+        self.rect = pygame.draw.rect(self.screen, (kCOLORS["blue"]), 
                                          (0, 0, self.width, 100))
-            self.draw_grid_flag=False
-            self.show_score(score)
-            self.show_selected_box(score, team)
-        # pygame.display.update()
+        self.draw_grid_flag=False
+        self.show_score(score)
+        self.show_selected_box(score, team)
 
-        cell_pos=self.width/self.num_columns
+        cell_width = self.width/self.num_columns
 
 
-        for row in range(self.num_columns):
-            cell_pos=self.width/self.num_columns
-            for x,header in enumerate(range(self.num_columns)):
+        for row in range(kNUM_ROWS + 2):
+            for col in range(self.num_columns):
                 self.rect = pygame.draw.rect(self.screen, (kCOLORS["black"]),
-                                             (0, row*self.row_height, 
-                                              cell_pos, self.row_height), 2)
-                cell_pos+=self.width/self.num_columns
+                                             (col*cell_width, row*self.row_height, 
+                                              cell_width, self.row_height), 2)
+
+                if col in self.board and row in self.board[col] and self.board[col][row] is None:
+                    self.clear_already_selected(col, row)
                 # pygame.display.update()
+        self.show_score(score)
         pygame.display.update()
 
     def clear_already_selected(self, col, row):
-        pygame.draw.rect(self.screen, (black), (row*(self.width/self.num_columns),
-                                                col*self.row_height,
-                                                self.width/self.num_columns,
-                                                self.row_height))
+        print("AS", col, row)
+        # Add one to row to account for category header
+        pygame.draw.rect(self.screen, (kCOLORS["black"]), (col*(self.width/self.num_columns),
+                         (row + 1)*self.row_height, self.width/self.num_columns, self.row_height))
         
     def show_score(self, scores):
         curser=10
         self.rect = pygame.draw.rect(self.screen, (kCOLORS["grey"]), (0,600 , self.width, 100))
         for team in scores:
-            self.screen.blit(self.font.render(team, True, (255,0,0)), (curser, 610))
+            self.screen.blit(self.font.render(str(team), True, (255,0,0)), (curser, 610))
             curser+=self.width/self.num_columns
         curser=10
         for team in scores:
@@ -103,42 +240,43 @@ class Pane(object):
 
     def show_selected_box(self, score, team):
         self.show_score(score)
-        self.rect = pygame.draw.rect(self.screen, (kCOLORS["red"]), (team*(self.width/6),600 , self.width/6, 100),3)
-        self.rect = pygame.draw.rect(self.screen, (kCOLORS["red"]), (team*(self.width/6),700 , self.width/6, 100))
+        self.rect = pygame.draw.rect(self.screen, (kCOLORS["red"]), ((team-1)*(self.width/6),600 , self.width/6, 100),3)
+        self.rect = pygame.draw.rect(self.screen, (kCOLORS["red"]), ((team-1)*(self.width/6),700 , self.width/6, 100))
 
     def display_text(self):
         for ii in self.board:
             if any(self.board[ii]):
-                self.add_text(ii, 0, self.categories[ii - 1])
+                self.add_text(ii, -1, self.categories[ii])
 
-            row = 0
             for jj in self.board[ii]:
-                row += 1
-                print(self.board[ii][jj])
-                if self.board[ii][jj]:
-                    print(ii, row)
-                    self.add_text(ii, row, self.board_values[row - 1])
+                if self.board[ii][jj] is not None:
+                    # These arrays are zero indexed, so we add one to what human sees
+                    self.add_text(ii, jj, "%i\n(%s%i)" % (self.board_values[jj], kCOLUMN_LETTERS[ii], jj + 1))
         pygame.display.update()
 
-        
     def add_text(self, column, row, text):
+        lines = textwrap.wrap(str(text), self.char_width)
+        
         # print(pos,text)
         x = column * self.width/self.num_columns + self.text_offset_x
-        y= self.row_height * row + self.text_offset_y
+        
+        # Add one to the row to account for category header
+        y= self.row_height * (row + 1) + self.text_offset_y
         color = kCOLORS["white"]
 
-        if row == 0:
+        if row < 0:
             color=kCOLORS["yellow"]
-        print("RENDER", x, y, text)
-        self.screen.blit(self.font.render(str(text), True, color), (x, y))
+
+        offset = 0
+        for ii in lines:
+            self.screen.blit(self.font.render(str(ii), True, color), (x, y + offset))
+            offset += self.text_line_height
 
 
+
+            
 if __name__ == "__main__":
     from time import sleep
 
-    pane = Pane()
-    pane.draw_grid({"A": 0, "B": 0}, 1)
-    pane.load_questions("study.csv")
-    pane.start_round(1)
-    pane.display_text()
-    sleep(10)
+
+    game_loop({1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0})
